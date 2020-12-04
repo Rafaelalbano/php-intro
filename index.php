829dd254fd4c125f9cc58de07a09ea9511245bfc
Rafael Albano <74366109+Rafaelalbano@users.noreply.github.com>
2020-12-03 22:46:59 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,37 +1,18 @@
-<?php
-
-$categorias = [];
-$categorias[] = 'infantil';
-$categorias[] = 'adolescente';
-$categorias[] = 'adulto';
-//print_r($categorias)
-
-$nome = 'Eduardo';
-$idade = 8;
-//var_dump($nome);
-//var_dump($idade);
-
-if($idade >= 6 && $idade <= 12)
-{
-    for($i = 0; $i <= count ($categorias); $i++)
-    {
-        if($categorias[$i] == 'infantil')
-            echo 'O nadador '.$nome. ' compete na categoria infantil';
-    }
-}
-else if($idade >= 13 && $idade <= 18)
-{
-    for($i = 0; $i <= count ($categorias); $i++)
-    {
-        if($categorias[$i] == 'adolescente')
-            echo 'O nadador ' .$nome. ' compete na categoria adolescente';
-    }
-}
-else
-{
-    for($i = 0; $i <= count ($categorias); $i++)
-    {
-        if($categorias[$i] == 'adulto')
-            echo 'O nadador '.$nome. ' compete na categoria adulto';
-    }
-}
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta charset="utf=8">
+      <title>Formulário de inscrição</title>
+      <meta name="author" content="">
+      <meta name="description" content="">
+      <meta name="viewport" content="widtch=device-widtch, initial-scale=1">
+  </head>
+  <body>
+  <p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+  <form action="script.php" method="post">
+    <p>Seu nome: <input type="text" name="nome" /></p>
+    <p>Sua idade: <input type="text" name="idade" /></p>
+    <p><input type="submit" value='Enviar dados do competidor' /></p>
+  </form>
+  </body>
+</html>
